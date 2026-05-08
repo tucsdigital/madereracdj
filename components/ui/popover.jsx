b1dc6412d45a4e41f6a6612d35f8b0582c1a7cda
tucsdigital <tucsdigital@gmail.com>
2026-05-08 09:18:16 -0300

    solucion a el calendario ui

--- components/ui/popover.jsx
+++ components/ui/popover.jsx
@@ -16,7 +16,7 @@ const PopoverContent = React.forwardRef(
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          "z-999 w-72 rounded-md border border-solid border-border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          "z-[10000] w-72 rounded-md border border-solid border-border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           className
         )}
         {...props}
@@ -59,7 +59,7 @@ const CustomPopover = ({
       {open && (
         <div
           className={cn(
-            "custom-popover-container absolute  left-0  z-999 w-56 bg-popover border border-border divide-y divide-default-100 rounded-md shadow-lg  focus:outline-hidden",
+            "custom-popover-container absolute left-0 z-[10000] w-56 bg-popover border border-border divide-y divide-default-100 rounded-md shadow-lg focus:outline-hidden",
             className
           )}
         >
